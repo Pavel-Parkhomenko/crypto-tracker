e9d8e1980ec53be8c6157f5cc274b9ce04944acb
pawelparhomenko@gmail.com <pawelparhomenko@gmail.com>
2023-03-07 19:25:10 +0300

    refactoring code

--- src/Coin.js
+++ src/Coin.js
@@ -13,8 +13,14 @@ const Coin = ({
   return (
     <div className='coin-container'>
       <div className='coin-row'>
+        <div className='coin'>
+          <img src={image} alt='crypto' />
+          <h1>{name}</h1>
+          <p className='coin-symbol'>{symbol}</p>
+        </div>
         <div className='coin-data'>
           <p className='coin-price'>${price}</p>
+      
           <p className='coin-volume'>${volume.toLocaleString()}</p>
 
           {priceChange < 0 ? (
@@ -25,18 +31,8 @@ const Coin = ({
             <p className='coin-percent green'>{priceChange.toFixed(2)}%</p>
       
       )}
-          <p>Price: </p>  
-          <div className='coin'>
-          <img src={image} alt='crypto' />
-          <h1>{name}</h1>
-          <p className='coin-symbol'>{symbol}</p>
-      
-        </div>
-
           <p className='coin-marketcap'>
-
             Mkt Cap: ${marketcap.toLocaleString()}
-
           </p>
         </div>
       </div>
