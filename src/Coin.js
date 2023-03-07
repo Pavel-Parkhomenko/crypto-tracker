1c3ffc5491b1ca3d426af356435b2dd50607b440
pawelparhomenko@gmail.com <pawelparhomenko@gmail.com>
2023-03-07 15:51:16 +0300

    change styles and coin.js

--- src/Coin.js
+++ src/Coin.js
@@ -12,23 +12,9 @@ const Coin = ({
 }) => {
   return (
     <div className='coin-container'>
-
       <div className='coin-row'>
-      
-        <div className='coin'>
-      
-          <img src={image} alt='crypto' />
-      
-          <h1>{name}</h1>
-      
-          <p className='coin-symbol'>{symbol}</p>
-      
-        </div>
-      
         <div className='coin-data'>
-      
           <p className='coin-price'>${price}</p>
-      
           <p className='coin-volume'>${volume.toLocaleString()}</p>
 
           {priceChange < 0 ? (
@@ -39,7 +25,13 @@ const Coin = ({
             <p className='coin-percent green'>{priceChange.toFixed(2)}%</p>
       
       )}
-
+          <p>Price: </p>  
+          <div className='coin'>
+          <img src={image} alt='crypto' />
+          <h1>{name}</h1>
+          <p className='coin-symbol'>{symbol}</p>
+      
+        </div>
 
           <p className='coin-marketcap'>
 
